fa0daf197f5141fea1dce179c8207ec2c5defca1
merluu <ci.beyer@duocuc.cl>
2025-11-09 23:37:51 -0300

    Se  agrega la base del proyecto web de reportería

---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/AppWebRutafit/src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import AppLayout from "./layouts/AppLayout";
+import Users from "./pages/Users";
+import RoutesPage from "./pages/RoutesPage";
+import Events from "./pages/Eventos";
+import Settings from "./pages/Settings";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <Routes>
+      {/* Login como inicio */}
+      <Route path="/" element={<Login />} />
+
+      {/* Área de la app con layout */}
+      <Route path="/app" element={<AppLayout />}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
+        <Route path="routes" element={<RoutesPage />} />
+        <Route path="events" element={<Events />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/AppWebRutafit/src/pages/Settings.tsx
@@ -0,0 +1,7 @@
+export default function Settings() {
+    return (
+        <div className="border rounded-2xl bg-white p-6 text-gray-500">
+            Sección <b>Configuración</b> (vacía para trabajar con el equipo)
+        </div>
+    );
+}
